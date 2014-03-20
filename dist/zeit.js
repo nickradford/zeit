7c8623f58a053a264bf238ce99b3642b2c7a47b3
Alexander Harding <alexander.harding@software.dell.com>
2014-03-20 00:35:37 -0500

    I messed up the last commit... Here's a fix

---
+++ dist/zeit.js
@@ -0,0 +1,3 @@
+var testing;
+
+testing = true;
--- test/test.js
+++
@@ -1,7 +0,0 @@
-describe('something', function(done) {
-  return describe("when given something", function() {
-    return it("should do this", function() {
-      return testing.should.equal(true);
-    });
-  });
-});
